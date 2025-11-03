c24a2f948049d795a8e97addc15b91e72201efc9
kevinariel <kevinariel.yap1@gmail.com>
2025-11-03 22:39:03 +0700

    adding card for recent income and modified a bit of expense transactions index name used for the card

--- frontend/financial-tracker/src/components/Dashboard/RecentIncome.jsx
+++ frontend/financial-tracker/src/components/Dashboard/RecentIncome.jsx
@@ -3,14 +3,14 @@ import { LuArrowRight } from 'react-icons/lu'
 import TransactionInfoCard from '../Cards/TransactionInfoCard'
 import moment from 'moment'
 
-const ExpenseTransactions = ({ transactions, onSeeMore }) => {
+const RecentIncome = ({ transactions, onSeeMore }) => {
     return (
         <div className="card">
             <div className="flex items-center justify-between">
-                <h5 className="text-lg">Expenses</h5>
+                <h5 className="text-lg">Income</h5>
 
-                <button className="card-btn" onClick={onSeeMore}>
-                    See All <LuArrowRight className="text-base"></LuArrowRight>
+                <button className='card-btn' onClick={onSeeMore}>
+                    See All <LuArrowRight className="text-base" />
                 </button>
             </div>
 
@@ -18,17 +18,17 @@ const ExpenseTransactions = ({ transactions, onSeeMore }) => {
                 {transactions?.slice(0, 5)?.map((item) => (
                     <TransactionInfoCard
                         key={item.id}
-                        title={item.category}
+                        title={item.type == 'income' ? item.category : item.source}
                         icon={item.icon}
                         date={moment(item.date).format("Do MMM YYYY")}
                         amount={item.amount}
-                        type="expense"
+                        type="income"
                         hideDeleteBtn
                     />
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ExpenseTransactions
+export default RecentIncome
