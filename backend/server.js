a7c39f37c9c6eca851da04d727faaace55fe75da
kevinariel <kevinariel.yap1@gmail.com>
2025-12-22 00:58:11 +0700

    hosting backend

--- backend/server.js
+++ backend/server.js
@@ -32,4 +32,9 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const mongoose = require("mongoose");   
 
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
 module.exports = app;   
