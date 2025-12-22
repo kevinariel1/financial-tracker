a87c9b682686cc8cc769ee9fee68f4cdc2fe8317
kevinariel <kevinariel.yap1@gmail.com>
2025-12-22 13:34:12 +0700

    Fixing image url upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,8 +13,9 @@ router.post("/uploadProfilePicture", upload.single("profilePicture"), (req, res)
         return res.status(400).json({ message: "No file uploaded" });
     }
 
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    const imageUrl = `/uploads/${req.file.filename}`; 
+    
     res.status(200).json({ imageUrl });
-})
+});
 
 module.exports = router;
