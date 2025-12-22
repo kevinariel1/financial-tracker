de240c91de67827a8a3a35b0fe78ce383863c0ad
kevinariel <kevinariel.yap1@gmail.com>
2025-12-22 13:17:41 +0700

    Another fixing

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.get("/", (req, res) => {
 app.use(cors({ 
   origin: process.env.CLIENT_URL || "*", 
   methods: ["GET", "POST", "PUT", "DELETE"], 
-  allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true,
 }));
 
