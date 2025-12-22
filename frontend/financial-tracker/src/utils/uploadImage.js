d0da7ecb94c79f4b5436d079b1ba84c3e82c2aa9
kevinariel <kevinariel.yap1@gmail.com>
2025-12-22 17:42:54 +0700

    Sign Up

--- frontend/financial-tracker/src/utils/uploadImage.js
+++ frontend/financial-tracker/src/utils/uploadImage.js
@@ -5,15 +5,14 @@ const uploadImage = async (imageFile) => {
   const formData = new FormData();
   formData.append("profilePicture", imageFile);
 
-  // 1. Manually check if the URL is correct
-  const baseURL = "https://financial-tracker-rjbe.vercel.app"; 
+  // Replace this with your EXACT Vercel backend URL
+  const baseURL = "https://financial-tracker-rjbe.vercel.app/"; 
   
   const response = await axios.post(
-    `${baseURL}${API_PATHS.AUTH.UPLOAD_IMAGE}`,
+    `${baseURL}${API_PATHS.AUTH.UPLOAD_IMAGE}`, // This evaluates to /api/v1/auth/uploadProfilePicture
     formData,
     { 
-        headers: { "Content-Type": "multipart/form-data" },
-        withCredentials: false // Helps avoid some CORS issues on public routes
+        headers: { "Content-Type": "multipart/form-data" }
     }
   );
   return response.data;
